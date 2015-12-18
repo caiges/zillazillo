36f2885878c8989da080f8004377779db3818621
caiges <caige@fastmail.fm>
2015-12-18 08:43:46 -0700

    Add awesome crazy text

--- main.lua
+++ main.lua
@@ -1,3 +1,22 @@
+function love.load()
+  alpha = 0
+  alphaIncrementer = 1
+end
+
+function love.update()
+  if alpha == 255 then
+    alphaIncrementer = -1
+  elseif alpha == 0 then
+    alphaIncrementer = 1
+  end
+
+  alpha = alpha + 5 * alphaIncrementer
+end
+
 function love.draw()
-   love.graphics.print("Zilla", 400, 300)
+  love.graphics.setColor(102, 20, 99, 255)
+  love.graphics.print(alpha, 20, 20)
+
+  love.graphics.setColor(102, 20, 99, alpha)
+  love.graphics.print("ZillaZillo", 400, 300)
 end
